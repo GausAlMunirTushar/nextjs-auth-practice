2da8db06f76d5749e5d5a7a4b7e0e05ca0edc287
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2024-12-16 00:15:37 +0600

    Add JWT authentication to login API and update dependencies

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,54 @@
+import { connectDB } from "@/config/database";
+import User from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import sendEmail from "@/helpers/mailer";
+
+connectDB();
+
+export async function POST(req: NextRequest, res: NextResponse) {
+	try {
+		const { email, password } = await NextRequest.json();
+
+		const user = await User.findOne({ email });
+		if (user) {
+			return NextResponse.json({
+				message: "User already exists",
+			});
+		} else if (!user) {
+			return NextResponse.json({
+				message: "User not found",
+			});
+		}
+		const isPasswordMatch = await bcrypt.compare(password, user.password);
+		if (!isPasswordMatch) {
+			return NextResponse.json({
+				message: "Invalid credentials",
+			});
+		}
+		const payload = {
+			id: user._id,
+			username: user.username,
+			email: user.email,
+		};
+		const token = jwt.sign(payload, process.env.JWT_SECRET!, {
+			expiresIn: "1d",
+		});
+		const response = NextResponse.json({
+			success: true,
+			message: "Login successful",
+			payload,
+		});
+		response.cookies.set("token", token, {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === "production",
+			sameSite: "strict",
+		});
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json({
+			error: "Internal server error",
+		});
+	}
+}
